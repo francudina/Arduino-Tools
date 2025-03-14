b9555bacffbd379bd982488b05424f815ef23427
Fran Cudina <cudina.fran@gmail.com>
2025-03-14 17:03:27 +0100

    vpn comment

--- src/Client/VpnManager.cpp
+++ src/Client/VpnManager.cpp
@@ -1,32 +1,32 @@
-#if defined(USE_WIREGUARD)
-#include "WiFiManager.h"
+// #if defined(USE_WIREGUARD)
+// #include "WiFiManager.h"
 
-#if !defined(USE_WIFI)
-#define USE_WIFI
-#endif
+// #if !defined(USE_WIFI)
+// #define USE_WIFI
+// #endif
 
 
-bool VpnManager::initVPN(
-        const char* localIP, 
-        const char* privateKey, 
-        const char* remotePeerAddress, 
-        const char* remotePeerPublicKey, 
-        uint16_t remotePeerPort
-) {
-    // check connection
-    if (!WiFiManager::isWiFiConnected()) {
-        Serial.print("VpnManager: WiFi isn't Connected");
-        return false;
-    }
-    // configure time before
-    WiFiManager::initTime();
-    // init vpn client
-    return wg.begin(
-        IpUtils::ipFromString(localIP),     // IP address of the local interface
-        privateKey,                         // Private key of the local interface
-        remotePeerAddress,                  // Address of the endpoint peer.
-        remotePeerPublicKey,                // Public key of the endpoint peer.
-        remotePeerPort);                    // Port pf the endpoint peer.
-}
+// bool VpnManager::initVPN(
+//         const char* localIP, 
+//         const char* privateKey, 
+//         const char* remotePeerAddress, 
+//         const char* remotePeerPublicKey, 
+//         uint16_t remotePeerPort
+// ) {
+//     // check connection
+//     if (!WiFiManager::isWiFiConnected()) {
+//         Serial.print("VpnManager: WiFi isn't Connected");
+//         return false;
+//     }
+//     // configure time before
+//     WiFiManager::initTime();
+//     // init vpn client
+//     return wg.begin(
+//         IpUtils::ipFromString(localIP),     // IP address of the local interface
+//         privateKey,                         // Private key of the local interface
+//         remotePeerAddress,                  // Address of the endpoint peer.
+//         remotePeerPublicKey,                // Public key of the endpoint peer.
+//         remotePeerPort);                    // Port pf the endpoint peer.
+// }
 
-#endif
+// #endif
